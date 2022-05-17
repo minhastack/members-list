eb3bf4a91d4146bbe40b23d18213d82469b0b3b5
Jose Neto <joseneto180201@gmail.com>
2022-05-17 02:58:11 +0000

    cors adicionado

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3333;
+
+const cors = require('cors');
 const routes  = require('./src/routes');
+
+
+app.use(cors);
 app.use(routes);
 
 app.listen(port);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const routes = require("express").Router();
 const numberOfMemebers = require('../discord');
 
-routes.get('/', async (req, res)=>{
+routes.get('/members', async (req, res)=>{
     numberOfMemebers()
         .then(data => res.json({numberOfMembers : data}))
-
 });
 
 module.exports = routes;
